6d48fade1176b9de72d308278620b2f0517eb356
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-01-08 18:23:30 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="apoc",
-    version="0.6.0",
+    version="0.6.1",
     author="haesleinhuepf",
     author_email="robert.haase@tu-dresden.de",
     description="Accelerated Pixel and Object Classifiers based on OpenCL",
