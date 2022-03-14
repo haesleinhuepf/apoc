6fbbbe01e984fbd6697554b317968f74d05f9630
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-03-14 14:10:28 +0100

    update test

--- tests/test_table_row_classification.py
+++ tests/test_table_row_classification.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pytest
 
 feature_table_dict = {
     "area": np.array([10, 10, 30, 30, 10]),
@@ -9,43 +10,22 @@ feature_table_data_frame = pd.DataFrame(feature_table_dict)
 ground_truth = np.array([1, 1, 2, 2, 1])
 
 
-def test_table_row_classification_from_dict():
+@pytest.mark.parametrize("feature_table", [feature_table_dict, feature_table_data_frame])
+def test_table_row_classification(tmpdir, feature_table):
     """Test the TableRowClassifier when the train/predict data are
     a dictionary with keys for columns names and columns stored
     as numpy arrays.
     """
 
     import apoc
-    oc = apoc.TableRowClassifier()
+    opencl_file = tmpdir.join("test.cl")
+    oc = apoc.TableRowClassifier(opencl_file)
     oc.train(
-        feature_table=feature_table_dict,
+        feature_table=feature_table,
         gt=ground_truth,
         continue_training=False
     )
-    result = oc.predict(feature_table_dict, return_numpy=True)
+    result = oc.predict(feature_table, return_numpy=True)
 
     assert result.dtype == np.uint32
-
-    print(result)
-
-    assert np.allclose(ground_truth, result)
-
-
-def test_table_row_classification_from_dataframe():
-    """Test the TableRowClassifier when the train/predict data
-    are a pandas dataframe.
-    """
-    import apoc
-    oc = apoc.TableRowClassifier()
-    oc.train(
-        feature_table=feature_table_data_frame,
-        gt=ground_truth,
-        continue_training=False
-    )
-    result = oc.predict(feature_table_dict, return_numpy=True)
-
-    assert result.dtype == np.uint32
-
-    print(result)
-
     assert np.allclose(ground_truth, result)
