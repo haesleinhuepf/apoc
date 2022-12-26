d34f6b95384afdfc4da6dfc905122db89455f27e
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-12-26 18:32:10 +0100

    added label merger class and example notebook

--- apoc/__init__.py
+++ apoc/__init__.py
@@ -4,6 +4,7 @@ from ._object_segmenter import ObjectSegmenter
 from ._object_classifier import ObjectClassifier
 from ._probability_mapper import ProbabilityMapper
 from ._table_row_classifier import TableRowClassifier
+from ._label_merger import LabelMerger
 from ._utils import generate_feature_stack
 from ._utils import erase_classifier
 from ._utils import list_available_object_classification_features
